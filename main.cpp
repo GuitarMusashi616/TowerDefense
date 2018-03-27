31a6e44f0af84215cc21692c30e81312a83c2442
rbiessel <rbiessel@alaska.edu>
2018-03-27 15:40:44 -0800

    added main

---
+++ main.cpp
@@ -0,0 +1,35 @@
+
+#include <SFML/Audio.hpp>
+#include <SFML/Graphics.hpp>
+
+int main()
+{
+    // Create the main window
+    sf::RenderWindow window(sf::VideoMode(800, 600), "SFML window");
+
+    // Start the game loop
+    while (window.isOpen())
+    {
+        // Process events
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            // Close window: exit
+            if (event.type == sf::Event::Closed) {
+                window.close();
+            }
+
+            // Escape pressed: exit
+            if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Escape) {
+                window.close();
+            }
+        }
+
+        // Clear screen
+        window.clear();
+
+        // Update the window
+        window.display();
+    }
+    return EXIT_SUCCESS;
+}
