3df7adafb798984bdfe7089115afbb68d5eb5140
amwilliams24 <amwilliams24@alaska.edu>
2018-03-29 10:54:11 -0800

    Added Mob and Track
    works :D

--- main.cpp
+++ main.cpp
@@ -1,35 +1,65 @@
 
+#include <iostream>
 #include <SFML/Audio.hpp>
 #include <SFML/Graphics.hpp>
+using std::cout;
+using std::cin;
+using std::endl;
 
 int main()
 {
-    // Create the main window
-    sf::RenderWindow window(sf::VideoMode(800, 600), "SFML window");
+	// Create the main window
+	sf::RenderWindow window(sf::VideoMode(680, 500), "SFML window");
 
-    // Start the game loop
-    while (window.isOpen())
-    {
-        // Process events
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            // Close window: exit
-            if (event.type == sf::Event::Closed) {
-                window.close();
-            }
+	//for fixedUpdates
 
-            // Escape pressed: exit
-            if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Escape) {
-                window.close();
-            }
-        }
+	//load textures
+	sf::Texture t1, t2;
+	t1.loadFromFile("GrassTrack.png");
+	t2.loadFromFile("ship.png");
+	sf::Sprite background{ t1 }, ship{ t2 };
+	
 
-        // Clear screen
-        window.clear();
+	//reposition mob
+	//ship.setOrigin(143, 311);
 
-        // Update the window
-        window.display();
-    }
-    return EXIT_SUCCESS;
+	// Start the game loop
+	while (window.isOpen())
+	{
+		// Process events
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			// Close window: exit
+			if (event.type == sf::Event::Closed) {
+				window.close();
+			}
+
+			// Escape pressed: exit
+			if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Escape) {
+				window.close();
+			}
+
+			//figure out pixel x,y,z location of click
+			if (event.type == sf::Event::MouseButtonPressed) {
+				
+				int x{ sf::Mouse::getPosition(window).x }, y{sf::Mouse::getPosition(window).y };
+				cout << x << ", " << y << endl;
+				ship.setPosition(sf::Vector2f{float(x), float(y) });
+				
+			}
+
+		}
+
+		// Clear screen
+		window.clear();
+
+		//draw to buffer
+		window.draw(background);
+		window.draw(ship);
+
+		// Update the window
+		window.display();
+	}
+	return EXIT_SUCCESS;
 }
